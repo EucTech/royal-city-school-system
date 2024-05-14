de94df9b7c3c317c1dcd0550d5a3b3eab2055e3b
EucTech <euccreative@gmail.com>
2024-05-14 21:55:54 +0100

    dashboard

--- schoolAdminDashboard/src/components/Dashboard/Overview.tsx
+++ schoolAdminDashboard/src/components/Dashboard/Overview.tsx
@@ -5,18 +5,18 @@ import { PiChalkboardTeacherFill, PiStudentFill } from "react-icons/pi";
 const Overview = () => {
 
     const overviewClass = classNames(
-        "flex flex-col items-center justify-center gap-3 w-[18em] h-[12em] p-3 border-[3px] border-[#430A5D] bg-white border-solid rounded-[30px]"
+        "flex flex-col items-center justify-center gap-3 w-[18em] h-[10em] sm:h-[12em] p-3 border-[3px] border-[#430A5D] bg-white border-solid rounded-[30px]"
     )
 
   return (
     <div className="flex flex-wrap items-center gap-3">
       <div className={` ${overviewClass}`}>
-        <PiStudentFill className="text-[80px] text-[#430A5D]" />
+        <PiStudentFill className="text-[50px] sm:text-[80px] text-[#430A5D]" />
         <h1 className="text-[17px] sm:text-[20px] text-[#322C2B] font-semibold">Total Students</h1>
         <p className="text-[15px] text-[#000] font-medium">2000</p>
       </div>
       <div className={` ${overviewClass}`}>
-      <PiChalkboardTeacherFill className="text-[80px] text-[#430A5D]" />
+      <PiChalkboardTeacherFill className="text-[50px] sm:text-[80px] text-[#430A5D]" />
         <h1 className="text-[17px] sm:text-[20px] text-[#322C2B] font-semibold">Total Teachers</h1>
         <p className="text-[15px] text-[#000] font-medium">100</p>
       </div>
