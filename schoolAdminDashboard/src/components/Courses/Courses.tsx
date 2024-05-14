61864c96002133ae1d0b01da445970c6f7042a2a
EucTech <euccreative@gmail.com>
2024-05-14 21:56:53 +0100

    added notice and courses sectio

--- schoolAdminDashboard/src/components/Courses/Courses.tsx
+++ schoolAdminDashboard/src/components/Courses/Courses.tsx
@@ -4,32 +4,33 @@ import { FaBook } from "react-icons/fa6";
 
 const Courses = () => {
   const overviewClass = classNames(
-    "flex flex-col items-center justify-center gap-3 w-[18em] h-[12em] p-3 border-[3px] border-[#430A5D] bg-white border-solid rounded-[30px]"
+    "flex flex-col items-center justify-center gap-3 w-[18em] h-[10em] sm:h-[12em] p-3 border-[3px] border-[#430A5D] bg-white border-solid rounded-[30px]"
   );
 
   return (
     <div className="flex flex-col gap-3 pt-10 p-6">
       <div className={` ${overviewClass}`}>
-        <FaBook className="text-[80px] text-[#430A5D]" />
+        <FaBook className="text-[50px] sm:text-[80px] text-[#430A5D]" />
         <h1 className="text-[17px] sm:text-[20px] text-[#430A5D] font-semibold">
           Total Subjects
         </h1>
         <p className="text-[15px] text-[#000] font-medium">100</p>
       </div>
 
-      <div className=" sm:w-[60%] w-full flex flex-col gap-7 mt-20">
+      <form className=" sm:w-[60%] w-full flex flex-col gap-7 mt-20">
         <h1 className=" text-center text-[17px] sm:text-[20px] font-semibold text-[#430A5D]">Add Subject</h1>
         <div className="flex flex-col gap-6">
           <div className="flex flex-col gap-3 ">
-            <label className="text-[#430A5D] text-[15px] font-medium" htmlFor="subject">Subject</label>
-            <input className=" border-2 border-solid border-[#E6E6E6] focus:outline-none focus:ring-2 focus:ring-[#430A5D] focus:border-transparent py-2 pl-3 rounded-lg" type="text" name="subject" id="subject" />
+            <label className="text-[rgb(67,10,93)] text-[15px] font-medium" htmlFor="subject">Subject</label>
+            <input placeholder="Enter the Subject" className=" border-2 border-solid border-[#E6E6E6] focus:outline-none focus:ring-2 focus:ring-[#430A5D] focus:border-transparent py-2 pl-3 rounded-lg" type="text" name="subject" id="subject" />
           </div>
           <div className="flex flex-col gap-3 ">
             <label className="text-[#430A5D] text-[15px] font-medium" htmlFor="class">Class</label>
-            <input className="  border-2 border-solid border-[#E6E6E6] focus:outline-none focus:ring-2 focus:ring-[#430A5D] focus:border-transparent py-2 pl-3 rounded-lg" type="text" name="class" id="class" />
+            <input placeholder="Enter the Class" className="  border-2 border-solid border-[#E6E6E6] focus:outline-none focus:ring-2 focus:ring-[#430A5D] focus:border-transparent py-2 pl-3 rounded-lg" type="text" name="class" id="class" />
           </div>
         </div>
-      </div>
+        <button className="bg-[#430A5D] text-white py-2 rounded-lg active:scale-95">Add Subject</button>
+      </form>
     </div>
   );
 };
--- schoolAdminDashboard/src/components/Notice/Notice.tsx
+++ schoolAdminDashboard/src/components/Notice/Notice.tsx
@@ -1,9 +1,38 @@
 // import React from 'react'
 
+import classNames from "classnames";
+import { GrAnnounce } from "react-icons/gr";
+
 const Notice = () => {
+
+  const overviewClass = classNames(
+    "flex flex-col items-center justify-center gap-3 w-[18em] h-[10em] sm:h-[12em] p-3 border-[3px] border-[#430A5D] bg-white border-solid rounded-[30px]"
+  );
+
   return (
-    <div>
-      <h1>Notice</h1>
+    <div className="flex flex-col gap-3 pt-10 p-6">
+      <div className={` ${overviewClass}`}>
+      <GrAnnounce className=" text-[50px] md:text-[80px] text-[#430A5D]" />
+        <h1 className="text-[17px] sm:text-[20px] text-[#430A5D] font-semibold">
+          Total Notices
+        </h1>
+        <p className="text-[15px] text-[#000] font-medium">100</p>
+      </div>
+
+      <form className=" sm:w-[60%] w-full flex flex-col gap-7 mt-20">
+        <h1 className=" text-center text-[17px] sm:text-[20px] font-semibold text-[#430A5D]">Add Announcement</h1>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-3 ">
+            <label className="text-[rgb(67,10,93)] text-[15px] font-medium" htmlFor="subject">Heading</label>
+            <input placeholder="Enter your heading" className=" border-2 border-solid border-[#E6E6E6] focus:outline-none focus:ring-2 focus:ring-[#430A5D] focus:border-transparent py-2 pl-3 rounded-lg" type="text" name="subject" id="subject" />
+          </div>
+          <div className="flex flex-col gap-3 ">
+            <label className="text-[#430A5D] text-[15px] font-medium" htmlFor="class">Notice</label>
+            <textarea className=" h-[10em] border-2 border-solid border-[#E6E6E6] focus:outline-none focus:ring-2 focus:ring-[#430A5D] focus:border-transparent py-2 pl-3 rounded-lg" name="notice" id="notice" placeholder="Write your notice"></textarea>
+          </div>
+        </div>
+        <button className="bg-[#430A5D] text-white py-2 rounded-lg active:scale-95">Submit</button>
+      </form>
     </div>
   )
 }
